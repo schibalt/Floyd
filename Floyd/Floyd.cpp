95d1d5c7766c2c3291c3529d4c17d541df3548b8
andrew <art26@zips.uakron.edu>
2013-10-11 14:56:06 -0400

    1st parallel progress chunk

--- Floyd/Floyd.cpp
+++ Floyd/Floyd.cpp
@@ -9,9 +9,10 @@
 #include <pthread.h>
 #include <sstream>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
-#include <Windows.h>
 #include <vector>
+#include <Windows.h>
 
 using std::cout;
 using std::endl;
@@ -22,8 +23,14 @@ using std::setw;
 using std::setfill;
 using std::size_t;
 using std::string;
-using std::vector;
 using std::to_string;
+using std::vector;
+using std::locale;
+using std::string;
+using std::stringstream;
+using std::istream_iterator;
+using std::cin;
+using std::wstring;
 
 #define NUM_THREADS     5
 HANDLE hConsole;
@@ -31,32 +38,32 @@ HANDLE hConsole;
 const int MAX_CHARS_PER_LINE = 512;
 const int MAX_TOKENS_PER_LINE = 20;
 const char* const DELIMITER = " ";
-vector<string> cities;
+vector<wstring> cities;
 int nCities;
+
+//let dist be a |V| × |V| array of minimum distances initialized to ∞ (infinity)
+int** dist ;
+int** next;
+
+int k;
+int rpp;
 
 void PrintDist (int** dist);
 void PrintDist (int** dist, int k, int i, int j);
 void PrintDistances (int** distances);
 void PrintNext (int** next);
 string Path (int** dist, int** next, int i, int j);
+void *Floyd_Row (void *thread_id);
+void Floyd_Row ();
+int64_t GetTimeMs64();
+vector<string> split (string const & input);
+vector<int> path (int source, int destination);
 
-/*
-void *TaskCode(void *argument)
-{
-int tid;
-
-tid = *((int *) argument);
-printf("Hello World! It's me, thread %d!\n", tid);
-
-// optionally: insert more useful stuff here
-
-return NULL;
-}
-*/
 int main (int argc, char* argv[])
 {
-    hConsole = GetStdHandle (STD_OUTPUT_HANDLE);
-    _setmode (_fileno (stdout), _O_U16TEXT);
+    //hConsole = GetStdHandle (STD_OUTPUT_HANDLE);
+    _setmode (_fileno (stdout), _O_U8TEXT);
+
 
     int testcase = atoi (argv[1]);
     bool bidirectional;
@@ -71,36 +78,33 @@ int main (int argc, char* argv[])
     switch (testcase)     //decide which case is going to be run
     {
     case 1:
-        fin.open ("simple.dat");   // open a file
+        fin.open (L"simple.dat");   // open a file
         break;
     default:
-        fin.open ("nqmq.dat");   // open a file
+        fin.open (L"nqmq.dat");   // open a file
     }//switch
 
     if (!fin.good())
+    {
+        wprintf (L"File for graph data couldn't be found or couldn't be open.");
         return 1; // exit if file not found
+    }
 
     // read an entire line into memory
     char buf[MAX_CHARS_PER_LINE];
     fin.getline (buf, MAX_CHARS_PER_LINE);
     istringstream (buf) >> nCities;
-    cities.push_back (buf);
+    cities.push_back (wstring (buf, buf + strlen (buf)));
 
     for (int city = nCities; city > 0; city--)
     {
         fin.getline (buf, MAX_CHARS_PER_LINE);
-        cities.push_back (buf);
+        cities.push_back (wstring (buf, buf + strlen (buf)));
     }
-    /*
-    for (vector<string>::iterator it = cities.begin() ; it != cities.end(); ++it) {
-        string city =  *it;
-        cout << city.c_str();
-    cout << '\n';
-    }
-    */
+
     for (int city = nCities; city > 0; city--)
     {
-        string svcity = cities[city ];
+        wstring svcity = cities[city ];
         for (unsigned i = 0; i < svcity.length(); ++i)
             wprintf (L"%c", svcity.at (i));
         wprintf (L"\n");
@@ -111,14 +115,14 @@ int main (int argc, char* argv[])
     int** distances = new int*[nCities + 1];
 
     //let dist be a |V| × |V| array of minimum distances initialized to ∞ (infinity)
-    int** dist = new int*[nCities + 1];
-    int** next = new int*[nCities + 1];
+    dist = new int*[nCities + 1];
+    next = new int*[nCities + 1];
 
     for (int i = nCities; i > 0; i--)     //for each edge (u,v)
     {
-        distances[i] = new int[nCities];
-        dist[i] = new int[nCities];//dist[u][v] ← w(u,v)  // the weight of the edge (u,v)
-        next[i] = new int[nCities];
+        distances[i] = new int[nCities + 1];
+        dist[i] = new int[nCities + 1];//dist[u][v] ← w(u,v)  // the weight of the edge (u,v)
+        next[i] = new int[nCities + 1];
 
         for (int j = nCities; j > 0; j--)
         {
@@ -142,8 +146,7 @@ int main (int argc, char* argv[])
     {
         fin.getline (buf, MAX_CHARS_PER_LINE);
 
-        if (buf[0] == '-')
-            break;
+        if (buf[0] == '-') break;
 
         istringstream iss (buf);
         int sourceCity = -1, destCity = -1, distance = INT_MAX;
@@ -153,29 +156,29 @@ int main (int argc, char* argv[])
             //(there's actually an additional substring that's either empty or whitespace)
             string sub;
             iss >> sub;
-            string svdestcity;
-            string svsourcecity;
+            wstring wsdestcity;
+            wstring wssourcecity;
 
             switch (subdex)
             {
             case 1:
                 distance = atoi (sub.c_str());
-                wprintf (L"distance: %i\n", distance);
+                wprintf (L"distance: %i\n\n", distance);
                 break;
             case 2:
                 destCity = atoi (sub.c_str());
                 wprintf (L"destination: ");
-                svdestcity = cities[destCity];
-                for (unsigned i = 0; i < svdestcity.length(); ++i)
-                    wprintf (L"%c", svdestcity.at (i));
+                wsdestcity = cities[destCity];
+                for (unsigned i = 0; i < wsdestcity.length(); ++i)
+                    wprintf (L"%c", wsdestcity.at (i));
                 wprintf (L"\n");
                 break;
             case 3:
                 sourceCity = atoi (sub.c_str());
                 wprintf (L"source: ");
-                svsourcecity = cities[sourceCity  ];
-                for (unsigned i = 0; i < svsourcecity.length(); ++i)
-                    wprintf (L"%c", svsourcecity.at (i));
+                wssourcecity = cities[sourceCity];
+                for (unsigned i = 0; i < wssourcecity.length(); ++i)
+                    wprintf (L"%c", wssourcecity.at (i));
                 wprintf (L"\n");
                 break;
             default:
@@ -204,69 +207,183 @@ int main (int argc, char* argv[])
     PrintDist (dist);
     PrintNext (next);
 
-    for (int k = 1; k <= nCities; k++)
+    for (int experiments = 1; experiments > 0; experiments--)
     {
-        //system("cls");
-        for (int i = 1; i <= nCities; i++)
+        int rc = 0;
+
+        //run loop
+        int p[] = {1, 2, 4, 7};
+        for (int np = 0; np < 4; np++) //runs the algorithm for
+            //particular number of processing elements
         {
-            for (int j = 1; j <= nCities; j++)
+
+            rpp = nCities / p[np]; //rows per processor
+            vector<pthread_t> threads (p[np]);
+            wprintf (L"────────────────────────────────────────────\n%i cities / %i threads = %i cities per thread\n────────────────────────────────────────────\n",
+                     nCities, p[np], rpp);
+            uint64_t runstarttime = GetTimeMs64();
+
+            for (k = 1; k <= nCities; k++)
             {
-				if(j==nCities && i%2==0)
-                PrintDist (dist, k, i, j);
-                int polldist ;
-                int offsetbycolumn = dist[i][k];
-                int offsetbyrow = dist[k][j];
-
-                //the sum of a negative weight edge and an infinite weight edge only
-                //seems less than an infinite weight edge because of the use of int data type.
-                //in principle the sum is still infinite.
-                if (offsetbyrow == INT_MAX || offsetbycolumn == INT_MAX)
-                    polldist = INT_MAX;
+                if (p[np] == 1)
+                    Floyd_Row();
                 else
-                    polldist = dist[i][k] + dist[k][j];
-
-                if (polldist < dist[i][j])
                 {
-                    dist[i][j] = polldist;
-                    next[i][j] = k ;
+                    //wprintf (L"──────\nk = %i\n──────\n", k);
+                    //system(L"cls");
+
+                    for (int thrid = 0; thrid < p[np]; thrid++)
+                        rc = pthread_create (&threads[thrid], NULL, Floyd_Row, (void *) thrid);
+
+                    for (int thrid = 0; thrid < p[np]; thrid++)
+                        rc = pthread_join (threads[thrid], NULL);
                 }
-            } //j
-        }//i
-    }//k
+            }//k
+            uint64_t runendtime = GetTimeMs64();
+            uint64_t runtime = runendtime - runstarttime;
+
+            wprintf (L"run time for %i processors: %d ns\n", p[np], runtime);
+            //PrintDist (dist);
+        }//np
+        //run loop end
+    }
 
     PrintNext (next);
-    string path = Path (dist, next, 4, 3);
 
-    for (int x = 0; x <= nCities; x++)
+    /* initialize random seed: */
+    srand ( (unsigned int) time (NULL));
+    string buffer;
+    /*
+        while (buffer != "n" )
+        {*/
+    int source = rand() % nCities + 1;
+    int destination = rand() % nCities + 1;
+
+    while (source == destination)
+        destination = rand() % nCities + 1;
+
+    vector<int> vecpath = path (source, destination);
+    wprintf (L"\nShortest path from %s to %s is %i.\n", cities[source].c_str(), cities[destination].c_str(), dist[source][destination]);
+
+    for (vector<int>::size_type i = 0; i < vecpath.size() - 1; i++)
     {
-        delete [] distances[x] ;
-        delete [] dist[x] ;
-        delete [] next[x] ;
+        int source = vecpath[i];
+        int destination = vecpath[i + 1];
+        wprintf (L"    %s to %s:\t%i\n", cities[source].c_str(), cities[destination].c_str(), dist[source][destination]);
+    }
+    wprintf (L"\nRun again? (y/n)\n", cities[source].c_str(), cities[destination].c_str(), dist[source][destination]);
+    /*
+    	cin >> buffer;
+
+    while(buffer != "y" && buffer != "n")
+    	cin >> buffer;
+    }*/
+
+    for (int x = 1; x <= nCities; x++)
+    {
+        delete [] distances[x];
+        delete [] dist[x];
+        delete [] next[x];
     }
     delete [] distances;
     delete [] dist;
     delete [] next;
-    /*
-    pthread_t threads[NUM_THREADS];
-    int thread_args[NUM_THREADS];
-    int rc, i;
-
-    // create all threads
-    for (i = 0; i < NUM_THREADS; ++i) {
-    thread_args[i] = i;
-    printf("In main: creating thread %d\n", i);
-    rc = pthread_create(&threads[i], NULL, TaskCode, (void *) &thread_args[i]);
-    assert(0 == rc);
-    }
-
-    // wait for all threads to complete
-    for (i = 0; i < NUM_THREADS; ++i) {
-    rc = pthread_join(threads[i], NULL);
-    assert(0 == rc);
-    }
-    */
-
     exit (EXIT_SUCCESS);
+}
+
+vector<int> path (int source, int destination)
+{
+    string wspath = Path (dist, next, source, destination);
+    //wprintf (L"\npath: %i%ls%i\n", source, wspath.c_str(), destination);
+    const string spath (wspath.begin(), wspath.end());
+    vector<int> pathvector;
+    char buffer[3];
+
+    string scity (buffer);
+
+    pathvector.push_back (source);
+    vector<string> intermediatepath = split (spath);
+
+    for (vector<string>::iterator it = intermediatepath.begin(); it != intermediatepath.end(); ++it)
+    {
+        int city = atoi ( (*it).c_str());
+        pathvector.push_back (city);
+    }
+
+    _itoa_s (destination, buffer, 10);
+    scity = string (buffer);
+    pathvector.push_back (destination);
+
+    return pathvector;
+}
+
+vector<string> split (string const & input)
+{
+    istringstream buffer (input);
+    vector<string> ret;
+
+    copy (istream_iterator<string> (buffer), istream_iterator<string>(), back_inserter (ret));
+    return ret;
+}
+
+void *Floyd_Row (void * thread_id)
+{
+    //wprintf (L"Thread %d says, \"Hello!\"\n", thread_id);
+
+    for (int i = (int) thread_id * rpp + 1; i < ( (int) thread_id + 1) * rpp + 1; i++)
+    {
+        //wprintf (L"processing row %i in thread %d\n", i, thread_id);
+        for (int j = 1; j <= nCities; j++)
+        {
+            int polldist;
+            int offsetbycolumn = dist[i][k];
+            int offsetbyrow = dist[k][j];
+
+            //the sum of a negative weight edge and an infinite weight edge only
+            //seems less than an infinite weight edge because of the use of int data type.
+            //in principle the sum is still infinite.
+            if (offsetbyrow == INT_MAX || offsetbycolumn == INT_MAX)
+                polldist = INT_MAX;
+            else
+                polldist = dist[i][k] + dist[k][j];
+
+            if (polldist < dist[i][j])
+            {
+                dist[i][j] = polldist;
+                next[i][j] = k;
+            }
+        } //j
+    }//i
+    pthread_exit (NULL);
+    return NULL;
+}
+
+void Floyd_Row ()
+{
+    for (int i = 1; i <= nCities; i++)
+    {
+        for (int j = 1; j <= nCities; j++)
+        {
+            //PrintDist (dist, k, i, j);
+            int polldist ;
+            int offsetbycolumn = dist[i][k];
+            int offsetbyrow = dist[k][j];
+
+            //the sum of a negative weight edge and an infinite weight edge only
+            //seems less than an infinite weight edge because of the use of int data type.
+            //in principle the sum is still infinite.
+            if (offsetbyrow == INT_MAX || offsetbycolumn == INT_MAX)
+                polldist = INT_MAX;
+            else
+                polldist = dist[i][k] + dist[k][j];
+
+            if (polldist < dist[i][j])
+            {
+                dist[i][j] = polldist;
+                next[i][j] = k;
+            }
+        } //j
+    }//i
 }
 
 string Path (int** dist, int** next, int i, int j)
@@ -274,16 +391,20 @@ string Path (int** dist, int** next, int i, int j)
     if (dist[i][j] == INT_MAX)
         return "no path";//no path
 
-    string intermediate = " ";
-    intermediate = to_string (next[i][j]);
-    if (intermediate == "-1")
+    string sintermediate = " ";
+    sintermediate = next[i][j];
+    int intermediate = next[i][j];
+
+    if (intermediate == -1)
         return " ";   // the direct edge from i to j gives the shortest path
     else
     {
-        string pathiint = Path (dist, next, i, atoi (intermediate.c_str()));
-        string pathintj = Path (dist, next, atoi (intermediate.c_str()), j);
+        string pathiint = Path (dist, next, i, intermediate);
+        string pathintj = Path (dist, next, intermediate, j);
+        stringstream sstm;
+        sstm << pathiint << intermediate << pathintj;
 
-        return pathiint + intermediate + pathintj;
+        return sstm.str();
     }
 }
 
@@ -322,7 +443,7 @@ void PrintDist (int** dist, int k, int i, int j)
     //print dist matrix
     wprintf (L"\n");
     wprintf (L"Shortest paths matrix (k = %i, i = %i, j = %i)\n\n", k, i, j);
-    wprintf (L"   │");
+    wprintf (L"   |");
     for (int x = 1; x <= nCities; x ++)
         wprintf (L" %04i ", x);
     wprintf (L"\n");
@@ -332,6 +453,7 @@ void PrintDist (int** dist, int k, int i, int j)
     wprintf (L"\n");
     for (int y = 1; y <= nCities; y ++)
     {
+        //wprintf (L"%02i |", y);
         wprintf (L"%02i │", y);
         for (int x = 1; x <= nCities; x ++)
         {
@@ -351,6 +473,7 @@ void PrintDist (int** dist, int k, int i, int j)
                 SetConsoleTextAttribute (hConsole, 4);
 
             if (distance == INT_MAX)
+                //wprintf (L" inf  ");
                 wprintf (L"  \x221e   ");
             else
                 wprintf (L" %04i ", distance);
@@ -364,9 +487,9 @@ void PrintDist (int** dist, int k, int i, int j)
 void PrintDistances (int** distances)
 {
     //print distances matrix
-    wprintf (L"\n");
-    wprintf (L"Initial distances matrix\n\n");
-    wprintf (L"   │");
+    wprintf (L"\nInitial distances matrix\n\n");
+    wprintf (L"   |");
+    //wprintf (L"   │");
     for (int x = 1; x <= nCities; x ++)
         wprintf (L" %03i ", x);
     wprintf (L"\n");
@@ -382,7 +505,8 @@ void PrintDistances (int** distances)
             int distance = distances[x][y];
 
             if (distance == INT_MAX)
-                wprintf (L"  \x221e  ");
+                wprintf (L" inf ");
+            //wprintf (L"  \x221e  ");
             else
                 wprintf (L" %03i ", distance);
         }
@@ -397,11 +521,11 @@ void PrintNext (int** next)
     wprintf (L"Path matrix\n\n");
     wprintf (L"   │");
     for (int x = 1; x <= nCities; x++)
-        wprintf (L" %03i ", x);
+        wprintf (L" %02i ", x);
     wprintf (L"\n");
     wprintf (L"───┼");
     for (int x = 1; x <= nCities; x++)
-        wprintf (L"─────");
+        wprintf (L"────");
     wprintf (L"\n");
     for (int x = 1; x <= nCities; x++)
     {
@@ -410,11 +534,46 @@ void PrintNext (int** next)
         {
             int nextcity = next[x][y];
 
-            if (nextcity == INT_MAX)
-                wprintf (L"  \x221e  ");
+            if (nextcity == -1)
+                wprintf (L" \u00D7  ");
             else
-                wprintf (L" %03i ", nextcity);
+                wprintf (L" %02i ", nextcity);
         }
         wprintf (L"\n");
     }
+}
+
+int64_t GetTimeMs64()
+{
+#ifdef WIN32
+    /* Windows */
+    FILETIME ft;
+    LARGE_INTEGER li;
+
+    /* Get the amount of 100 nano seconds intervals elapsed since January 1, 1601 (UTC) and copy it
+    * to a LARGE_INTEGER structure. */
+    GetSystemTimeAsFileTime (&ft);
+    li.LowPart = ft.dwLowDateTime;
+    li.HighPart = ft.dwHighDateTime;
+
+    uint64_t ret = li.QuadPart;
+    ret -= 116444736000000000LL; /* Convert from file time to UNIX epoch time. */
+    ret /= 10; // From 100 nano seconds (10^-7)
+
+    return ret;
+#else
+    /* Linux */
+    struct timeval tv;
+
+    gettimeofday (&tv, NULL);
+
+    uint64 ret = tv.tv_usec;
+    /* Convert from micro seconds (10^-6) to milliseconds (10^-3) */
+    ret /= 1000;
+
+    /* Adds the seconds (10^0) after converting them to milliseconds (10^-3) */
+    ret += (tv.tv_sec * 1000);
+
+    return ret;
+#endif
 }
